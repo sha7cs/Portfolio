ebbd4f6fd5f84a35aa1de536669d2a2a8b36bacb
shatha <sha7cs@gmail.com>
2025-09-14 01:48:19 +0300

    submitting project

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,39 @@
 function init() {
     const sections = document.querySelectorAll(".section")
-    console.log(sections)
-    const colors = ['#F296BD', '#FCCA59', '#99B7F5', '#267F53']
+    const body = document.querySelector("body")
+    const aboutMe = document.querySelector('#about-me')
 
-    const observer = new IntersectionObserver((entries)=> {
-        console.log(entries)
+    let color = ''
+
+    const observer = new IntersectionObserver((entries) => {
+        /* i saw this youtube video to learn it https://www.youtube.com/watch?v=Jo8ABAJtMM0 
+        and i read the docs for more info */
+
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
+                console.log(entry)
                 entry.target.classList.add('show')
+                if (color !== '')
+                    body.classList.remove(color)
+                color = entry.target.attributes[2].value
+                body.classList.add(color)
             } else {
                 entry.target.classList.remove('show')
             }
-        }, { threshold: 0.7})
-
+        }, { threshold: 0.7 })
     })
-
     sections.forEach(section => observer.observe(section));
+
+    window.addEventListener("scroll", () => { // i tried to do this with the intersection observer but it wasnt working how i want it to
+        // so i looked up another way to observe the scroll and i found this in the docs, and watched this video https://www.youtube.com/watch?v=V9CY0F4Wc7M        
+        
+        if (window.scrollY > 320) { // 300 makes it not responsive
+            aboutMe.style.width = "35vw";
+        } else {
+            aboutMe.style.width = "100%";
+        }
+    });
+
 }
-/* i saw this youtube video to learn it https://www.youtube.com/watch?v=Jo8ABAJtMM0 
-and i read the docs for more info */
+
 document.addEventListener('DOMContentLoaded', init)
